a002b8851bec2dca735c9867f2de0bda18dd4cd4
ahmed-M32 <am.3443rrrr@gmail.com>
2024-07-02 13:24:47 +0300

    final commit hopfully

--- api/index.js
+++ api/index.js
@@ -1,28 +1,55 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 const app = express();
+
+const email1 = "am.3443rrrr@gmail.com";
+const pass = "wiqsjsacziorvhtt";
 
 const port = process.env.port || 3000;
 
 var corsOptions = {
-    origin: 'http://127.0.0.1:5500',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+	origin: "https://portfolio-ahmed-m32.vercel.app/",
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-  let arr =[ ]
-app.use(express.json())
-app.use(cors(corsOptions))
-   
-app.get('/',(req,res)=>{
-    res.send("wel")
-})
+app.use(express.json());
+app.use(cors(corsOptions));
 
-app.post('/',(req,res)=>{
-    console.log(req.body);
-    arr.push(req.body)
-    req.body["mad"]=3
-    res.json({ message: req.body });
-})
+app.get("/", (req, res) => {
+	res.send("wel");
+});
 
+app.post("/", (req, res) => {
+	console.log(req.body);
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		host: "smtp.gmail.com",
+		port: 465,
+		secure: true,
+		auth: {
+			user: email1,
+			pass: pass
+		},
+	});
 
-app.listen(port,()=>{ console.log("its running mfs")})
+	const data = {
+		from: req.body.address,
+		to: email1,
+		subject: "Portfolio Email",
+		text: req.body.message
+	};
+
+	transporter.sendMail(data, (error, success) => {
+		if (error) {
+			console.log(error);
+			res.send("there was a problem");
+		} else if (success) {
+			console.log("success");
+			res.send("email sent " + success.response);
+		}
+	});
+});
+
+app.listen(port, () => {
+});
